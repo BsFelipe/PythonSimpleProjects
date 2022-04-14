2b6caa8d37ab679a65c0d1e59630b283011bce2e
FelipeBs <felipe.bsilvaa@gmail.com>
2022-04-14 07:25:48 -0300

    convert json to csv

--- JsonToCSV/convert.py
+++ JsonToCSV/convert.py
@@ -5,6 +5,7 @@ if __name__ == '__main__':
         with open('test.json', 'r') as f:
             data = json.loads(f.read())
 
+        # TODO convert dynamic fields
         output = ';'.join([*data[0]])
         for obj in data:
             output += f'\n{obj["Name"]};{obj["age"]};{obj["birthyear"]}'
